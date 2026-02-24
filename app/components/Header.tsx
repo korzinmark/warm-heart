53d2724901ba802b9bff8b3b789bac9225f117fa
Mark Korzin <foxy@mm.local>
2026-02-24 19:53:52 +0200

    Finishing popular products section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,47 +3,48 @@ import { Menu, ShoppingBag, Search } from 'lucide-react'
 
 export default function Header() {
     return (
-        <header>
-            <div className="container mx-auto p-5 lg:py-7">
-                <div className="grid grid-cols-3 items-center">
-                    <div className="flex items-center gap-3">
-                        <button type="button" className="xl:hidden p-2" aria-label="Open menu">
-                            <Menu />
-                        </button>
+        <header className="container mx-auto p-5 lg:py-7">
+            <div className="grid grid-cols-3 items-center">
+                <div className="flex items-center gap-3">
+                    <button type="button" className="p-2 xl:hidden" aria-label="Open menu">
+                        <Menu />
+                    </button>
 
-                        <nav aria-label="Primary" className="hidden xl:flex gap-12">
-                            <Link href="/shop" className="hover:underline">
-                                Shop
-                            </Link>
-                            <Link href="/about" className="hover:underline">
-                                About us
-                            </Link>
-                            <Link href="/contacts" className="hover:underline">
-                                Contacts
-                            </Link>
-                        </nav>
-                    </div>
-
-                    <Link href="/" className="justify-self-center text-2xl font-semibold uppercase">
-                        <span className="sm:hidden">wh</span>
-                        <span className="hidden sm:inline md:text-3xl">warm heart</span>
-                    </Link>
-
-                    <div className="flex justify-self-end gap-8 lg:gap-12">
-                        <button
-                            type="button"
-                            className="flex gap-3 p-2 rounded-md hover:bg-black/5 cursor-pointer"
-                            aria-label="Search"
-                        >
-                            <Search className="hidden md:block" />
-                            <span className="hidden lg:inline">Search</span>
-                        </button>
-
-                        <Link href="/cart" className="flex gap-2 rounded-md p-2 hover:bg-black/5">
-                            <ShoppingBag />
-                            <span className="font-medium">(0)</span>
+                    <nav aria-label="Primary" className="hidden gap-12 xl:flex">
+                        <Link href="/shop" className="hover:underline">
+                            Shop
                         </Link>
-                    </div>
+                        <Link href="/about" className="hover:underline">
+                            About us
+                        </Link>
+                        <Link href="/contacts" className="hover:underline">
+                            Contacts
+                        </Link>
+                    </nav>
+                </div>
+
+                <Link href="/" className="justify-self-center text-2xl font-semibold uppercase">
+                    <span className="sm:hidden">wh</span>
+                    <span className="hidden sm:inline md:text-3xl">warm heart</span>
+                </Link>
+
+                <div className="flex items-center gap-8 justify-self-end lg:gap-12">
+                    <button
+                        type="button"
+                        className="flex cursor-pointer gap-3 rounded-md p-2 hover:bg-black/5"
+                        aria-label="Search"
+                    >
+                        <Search className="hidden md:block" />
+                        <span className="hidden lg:inline">Search</span>
+                    </button>
+
+                    <Link
+                        href="/cart"
+                        className="flex items-center gap-2 rounded-md p-2 hover:bg-black/5"
+                    >
+                        <ShoppingBag />
+                        <span className="font-medium">(0)</span>
+                    </Link>
                 </div>
             </div>
         </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
     return (
         <>
-            <section className="relative bg-beige">
+            <section className="bg-beige relative">
                 <div className="md:flex">
-                    <div className="container mx-auto px-5 pt-17 pb-22 md:pt-22 md:pb-30">
-                        <h1 className="text-4xl/12 sm:text-5xl/15 font-semibold text-left md:text-6xl/19">
+                    <div className="container mx-auto px-5 pt-17 pb-22 md:pt-22 md:pb-30 xl:pt-45.25 xl:pb-52.75">
+                        <h1 className="text-left text-4xl/12 font-semibold sm:text-5xl/15 md:text-6xl/19">
                             Soft plaids <br /> for your comfort
                         </h1>
 
@@ -15,46 +16,71 @@ export default function Home() {
                             and the atmosphere in the house will be warmer.
                         </p>
 
-                        <button className="mt-9 bg-black text-white py-3 px-20 rounded-full">
+                        <button className="mt-9 rounded-full bg-black px-20 py-3 text-white">
                             Shop now
                         </button>
                     </div>
 
-                    <div
-                        className="absolute left-1/2 -translate-x-1/2 sm:left-auto sm:translate-x-0 sm:right-25
-                                bottom-40 sm:bottom-41 z-1
-                                md:-bottom-14 md:right-55
-                                lg:right-35 lg:top-30
-                                lg:before:content-['']
-                                lg:before:absolute
-                                lg:before:-top-11
-                                lg:before:left-1
-                                lg:before:w-full
-                                lg:before:h-40
-                                lg:before:bg-[url('/top.svg')]
-                                lg:before:bg-no-repeat
-
-                                lg:after:content-['']
-                                lg:after:absolute
-                                lg:after:bottom-20
-                                lg:after:-right-54
-                                lg:after:w-full
-                                lg:after:h-40
-                                lg:after:bg-[url('/down.svg')]
-                                lg:after:bg-no-repeat
-                                "
-                    >
+                    <div className="absolute bottom-40 left-1/2 z-1 -translate-x-1/2 sm:right-25 sm:bottom-41 sm:left-auto sm:translate-x-0 md:right-55 md:-bottom-14 lg:top-30 lg:right-35 lg:before:absolute lg:before:-top-11 lg:before:left-1 lg:before:h-40 lg:before:w-30 lg:before:bg-[url('/top.svg')] lg:before:bg-no-repeat lg:before:content-[''] lg:after:absolute lg:after:-right-5 lg:after:bottom-20 lg:after:h-40 lg:after:w-30 lg:after:bg-[url('/down.svg')] lg:after:bg-no-repeat lg:after:content-[''] xl:top-40 xl:right-80 xl:before:left-5 xl:after:bottom-33 2xl:right-1/2 2xl:translate-x-100">
                         <Image
-                            loading="eager"
+                            preload
                             src="/promo-plaid.png"
                             width={467}
                             height={455}
                             alt="plaid"
-                            className="w-45 md:w-62.75 lg:w-78 h-auto"
+                            className="h-auto w-45 md:w-62.75 lg:w-78 xl:w-101.25"
                         />
                     </div>
 
-                    <div className="min-h-72 sm:min-h-59 md:min-w-1/3 lg:min-w-[23%] md:h-full md:absolute md:right-0 bg-black bg-[url(/promo-bg.png)]"></div>
+                    <div className="min-h-72 bg-black bg-[url(/promo-bg.png)] sm:min-h-59 md:absolute md:right-0 md:h-full md:min-w-1/3 lg:min-w-[23%] xl:min-w-[35%] 2xl:min-w-[40%]"></div>
+                </div>
+            </section>
+
+            <section>
+                <div className="container mx-auto px-5 py-17.5">
+                    <h2 className="text-4xl font-semibold">Popular products</h2>
+
+                    <div className="flex justify-center gap-5">
+                        <article className="mt-10 flex justify-center">
+                            <Link href="/product/gerhild">
+                                <Image
+                                    src="/pop-plaid-beige.png"
+                                    alt="Gerhild beige plaid 130x170 cm"
+                                    width={1090}
+                                    height={1049}
+                                    className="bg-beige h-80 w-80 rounded-3xl p-10 sm:h-52.5 sm:w-52.5 sm:p-7"
+                                />
+
+                                <h3 className="mt-2.5 text-xl font-medium">Gerhild</h3>
+
+                                <div className="flex justify-between">
+                                    <p>130x170 cm</p>
+
+                                    <p>€90</p>
+                                </div>
+                            </Link>
+                        </article>
+
+                        <article className="mt-10 hidden justify-center sm:flex">
+                            <Link href="/product/gerhild">
+                                <Image
+                                    src="/pop-plaid-gray.png"
+                                    alt="Gultall gray plaid 130x170 cm"
+                                    width={1090}
+                                    height={1049}
+                                    className="bg-beige h-52.5 w-52.5 rounded-3xl p-7"
+                                />
+
+                                <h3 className="mt-2.5 text-xl font-medium">Gultall</h3>
+
+                                <div className="flex justify-between">
+                                    <p>130x170 cm</p>
+
+                                    <p>€180</p>
+                                </div>
+                            </Link>
+                        </article>
+                    </div>
                 </div>
             </section>
         </>
